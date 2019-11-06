8522e208add76b0877c65473f4967c800f99c380
xzlcc <xzllcc@163.com>
2019-11-06 17:13:39 +0800

    qqq

---
+++ app/src/main/java/com/mz/xzlcc/MinaActivity.java
@@ -0,0 +1,16 @@
+package com.mz.xzlcc;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MinaActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+    }
+}
